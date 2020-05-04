67fafe60a24df3e935135e6e321e98254da642b1
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-05-04 22:32:35 +0545

    added download code

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -24,7 +27,16 @@ class WorkSingleAPIView(viewsets.ModelViewSet):
     queryset = MusicalWork.objects.all()
 
     def download_file(self):
-        return Response(dict(success='success'), status=200)
+        rows = self.get_queryset()
+        filename = 'musical-work-report.csv'
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        writer = csv.writer(response)
+        for row in rows:
+            # writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+            writer.writerow(row)
+        return response
 
     def list(self, request, *args, **kwargs):
         download = request.query_params.get('download', '')

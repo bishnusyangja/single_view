d237a2416e6a83ffcbe821847676cc4036dc26f7
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-05-05 23:05:13 +0545

    issue in front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import {Form, Input} from 'antd';
+import 'antd/dist/antd.css';
+
+import {Button} from 'antd';
 
 function App() {
 

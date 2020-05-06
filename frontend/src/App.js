75ea281f8fa6204e9f69dfa907622124bc30ba38
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-05-06 12:32:00 +0545

    added code in front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css'
-import {Row, Col, Form, Upload, Button } from 'antd';
+import {Row, Col, Form, Upload, Button, notification } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 
 
 function App() {
+
+    const notify_success = () => {
+      notification.open({
+        message: 'Register Successful !!',
+        description:
+          'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        onClick: () => {
+          console.log('Notification Clicked!');
+        },
+      });
+    };
+
+    const submitForm = () => {
+        let data = {};
+        Request().put('/upload-file/', data)
+          .then((response) => {
+            console.log("success")
+          })
+          .catch((error) => {
+            console.log("error in quiz submission")
+          })
+          .finally(() => {
+            console.log('finally block at quiz')
+        });
+    }
+
   return ( <div style={{margin:'20px'}}>
     <Row>
       <Col span={5} >
-         <Form>
+         <Form onFinish={submitForm}>
         <Form.Item> <Upload >
             <Button>
               <UploadOutlined /> Upload
